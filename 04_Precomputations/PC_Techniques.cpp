27a7db6e7467281dd92f78e7ce224eaea29d7649
Shree Mangalam <sshreemangalam@gmail.com>
2023-06-25 18:43:02 +0530

    added precomputation

--- 04_Precomputations/PC_Techniques.cpp
+++ 04_Precomputations/PC_Techniques.cpp
@@ -11,22 +11,25 @@ where M = 10^9+7
 
 #include <bits/stdc++.h>
 using namespace std;
-
+const int N = 1e5 + 10;
+const int M = 1e9 + 7;
+long long fact[N];
 int main()
 {
+  fact[0] = fact[1] = 1;
+  for (int i = 2; i < N; i++)
+  {
+    fact[i] = fact[i - 1] * i;
+  }
   int t;
   std::cin >> t;
   while (t--)
   {
     int n;
     std::cin >> n;
-    int M = 1e9 + 7;
-    long long fact = 1;
-    for (int i = 2; i <= n; ++i)
-    {
-      fact = (fact * i) % M;
-    }
-    cout << fact;
+    cout << fact[n];
   }
   return 0;
 }
+// old code complexity, O(T*N) = 10^10
+// new code complexity, O(N) + O(T) = 10^5 + 10^5 = 2*10^5 = 10^5
